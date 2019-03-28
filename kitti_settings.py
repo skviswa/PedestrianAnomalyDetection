af3f7664996f0ac67be432fb4d37a71e6fb87dba
Fathima Khazana <fathimakhazana@Fathimas-MacBook-Pro.local>
2019-03-28 14:10:22 -0400

    Processed Data

---
+++ kitti_settings.py
@@ -0,0 +1,10 @@
+# Where KITTI data will be saved if you run process_kitti.py
+# If you directly download the processed data, change to the path of the data.
+DATA_DIR = './ucsd_data/'
+
+# Where model weights and config will be saved if you run kitti_train.py
+# If you directly download the trained weights, change to appropriate path.
+WEIGHTS_DIR = './model_ucsd_data/'
+
+# Where results (prediction plots and evaluation file) will be saved.
+RESULTS_SAVE_DIR = './ucsd_results/'
---
+++ process_kitti.py
@@ -0,0 +1,58 @@
+'''
+Code for downloading and processing KITTI data (Geiger et al. 2013, http://www.cvlibs.net/datasets/kitti/)
+'''
+
+import os
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import numpy as np
+from imageio import imread
+from scipy.misc import imresize
+import hickle as hkl
+from kitti_settings import *
+import os
+
+desired_im_sz = (128, 160)
+
+
+if not os.path.exists(DATA_DIR): os.mkdir(DATA_DIR)
+
+def get_files(folder_name):
+    path = os.path.join(DATA_DIR,'UCSDped1',folder_name)
+    files = [i for i in os.listdir(path) if not i.startswith('.') and os.path.isdir(os.path.join(path,i))]
+    return files
+
+def process_data():
+    splits = {s: get_files(s) for s in ['Train', 'Test', 'Val']}    
+    for split in splits:
+        im_list = []
+        source_list = []  # corresponds to recording that image came from
+        for folder in splits[split]:
+            im_dir = os.path.join(DATA_DIR,'UCSDped1', split, folder)
+            files = [i for i in os.listdir(im_dir) if not i.startswith('.')]
+            im_list += [os.path.join(im_dir ,f) for f in sorted(files)]
+            source_list += [folder] * len(files)
+
+        print( 'Creating ' + split + ' data: ' + str(len(im_list)) + ' images')
+        X = np.zeros((len(im_list),) + desired_im_sz + (1,), np.uint8)
+        for i, im_file in enumerate(im_list):
+            im = imread(im_file)
+            X[i] = process_im(im, desired_im_sz)
+        
+
+        hkl.dump(X, os.path.join(DATA_DIR, 'X_' + split + '.hkl'))
+        hkl.dump(source_list, os.path.join(DATA_DIR, 'sources_' + split + '.hkl'))
+
+
+# resize and crop image
+def process_im(im, desired_sz):
+    target_ds = float(desired_sz[0])/im.shape[0]
+    im = imresize(im, (desired_sz[0], int(np.round(target_ds * im.shape[1]))))
+    d = int((im.shape[1] - desired_sz[1]) / 2)
+    im = im[:, d:d+desired_sz[1]]
+    return im.reshape(desired_sz + (1,))
+
+
+if __name__ == '__main__':
+    process_data()
