7f3525727b93f025108f0399623bef671de8147f
khazana <haiyum95@gmail.com>
2019-04-07 22:05:36 -0400

    Update kitti_train.py

--- kitti_train.py
+++ kitti_train.py
@@ -9,7 +9,7 @@ from six.moves import cPickle
 
 from keras import backend as K
 from keras.models import Model
-from keras.layers import Input, Dense, Flatten
+from keras.layers import Input, Dense, Flatten, Dropout
 from keras.layers import LSTM
 from keras.layers import TimeDistributed
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint, CSVLogger
