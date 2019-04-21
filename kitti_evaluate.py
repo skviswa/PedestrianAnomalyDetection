0748a7106468d95b9c44f74a1beff97615f92245
skviswa <karthik.kv.mst@gmail.com>
2019-04-20 20:25:15 -0400

    getting psnr metric

--- kitti_evaluate.py
+++ kitti_evaluate.py
@@ -5,6 +5,7 @@ Calculates mean-squared error and plots predictions.
 
 import os
 import sys
+import math
 import json
 import numpy as np
 from six.moves import cPickle
@@ -23,6 +24,10 @@ from kitti_settings import *
 from datetime import datetime
 from process_kitti import get_files
 from collections import defaultdict
+
+
+def psnr(mse, pk=255):
+    return 10 * math.log10(pk/mse)
 
 def get_test_splits(subdir):
     splits = {s: get_files(s, subdir) for s in ['Test']}    
@@ -79,7 +84,7 @@ def make_error_plot(err_dict, err_save_dir):
             
 n_plot = 40
 batch_size = 10
-nt = 20#20
+nt = 10#20
 
 subdir_model = 'UCSDped2' #'UCSDped2'
 subdir_test = 'UCSDped2' ##'UCSDped1' 'UCSDped2' 'total' 
@@ -132,18 +137,21 @@ mse_frame_sd_filename = 'mse_frame_sd.json'
 mse_prev_frame_sd_filename = 'mse_prev_frame_sd.json'
 mse_err_prev_frame_sd_filename = 'mse_err_prev_frame_sd.json'
 
-mse_videos_var_filename = 'mse_videos_var.json'
-mse_frame_var_filename = 'mse_frame_var.json'
-mse_prev_frame_var_filename = 'mse_prev_frame_var.json'
-mse_err_prev_frame_var_filename = 'mse_err_prev_frame_var.json'
+#mse_videos_var_filename = 'mse_videos_var.json'
+#mse_frame_var_filename = 'mse_frame_var.json'
+#mse_prev_frame_var_filename = 'mse_prev_frame_var.json'
+#mse_err_prev_frame_var_filename = 'mse_err_prev_frame_var.json'
 
+psnr_frame_filename = 'psnr_frame.json'
+psnr_prev_frame_filename = 'psnr_prev_frame.json'
 
 pred_save_dir = 'prediction_plots'
 err_save_dir = 'error_plots'
 err_prev_save_dir = 'prev_frame_plots'
 err_model_prev_save_dir = 'model_prev_frame_plots'
-var_save_dir = 'var_plots'
+#var_save_dir = 'var_plots'
 sd_save_dir = 'sd_plots'
+psnr_save_dir = 'psnr_plots'
 
 now = datetime.now
 folder_now = now().strftime("%Y_%m_%d-%H%M")
@@ -169,8 +177,11 @@ if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, er
 if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, err_model_prev_save_dir)):
     os.mkdir(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, err_model_prev_save_dir))
 
-if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir)):
-    os.mkdir(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir))
+#if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir)):
+#    os.mkdir(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir))
+
+if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, psnr_save_dir)):
+    os.mkdir(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, psnr_save_dir))
 
 if not os.path.exists(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, sd_save_dir)):
     os.mkdir(os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, sd_save_dir))
@@ -188,10 +199,13 @@ mse_frame_sd_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_
 mse_prev_frame_sd_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_prev_frame_sd_filename)
 mse_err_prev_frame_sd_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_err_prev_frame_sd_filename)
 
-mse_videos_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_videos_var_filename)
-mse_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_frame_var_filename)
-mse_prev_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_prev_frame_var_filename)
-mse_err_prev_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_err_prev_frame_var_filename)
+#mse_videos_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_videos_var_filename)
+#mse_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_frame_var_filename)
+#mse_prev_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_prev_frame_var_filename)
+#mse_err_prev_frame_var_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, mse_err_prev_frame_var_filename)
+
+psnr_frame_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, psnr_frame_filename)
+psnr_prev_frame_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, psnr_prev_frame_filename)
 
 overall_mse_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, overall_mse_filename)
 pred_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, pred_save_dir)
@@ -199,12 +213,9 @@ err_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, err_save
 err_prev_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, err_prev_save_dir)
 err_model_prev_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, err_model_prev_save_dir)
 
-var_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir)
+#var_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, var_save_dir)
 sd_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, sd_save_dir)
-
-##X_test = np.load(r'ucsd_results\UCSDped1\2019_04_03-2040\Xtest.npy')
-##X_hat = np.load(r'ucsd_results\UCSDped1\2019_04_03-2040\Xhat.npy')
-##plt.imshow(X_test[11,6], cmap='gray', interpolation='none')
+psnr_save_path = os.path.join(RESULTS_SAVE_DIR, subdir_test, folder_now, psnr_save_dir)
 
 if subdir_test == 'total':
     im_list, source_list = get_test_splits(['UCSDped1', 'UCSDped2'])
@@ -236,10 +247,13 @@ mse_model_frame_sd = defaultdict(list)
 mse_prev_frame_sd = defaultdict(list)
 mse_err_prev_frame_sd = defaultdict(list)
 
-mse_videos_var = dict()
-mse_model_frame_var = defaultdict(list)
-mse_prev_frame_var = defaultdict(list)
-mse_err_prev_frame_var = defaultdict(list)
+#mse_videos_var = dict()
+#mse_model_frame_var = defaultdict(list)
+#mse_prev_frame_var = defaultdict(list)
+#mse_err_prev_frame_var = defaultdict(list)
+
+psnr_model_frame = defaultdict(list)
+psnr_prev_frame = defaultdict(list)
 
 i = 0
 for k,v in sorted(possible_starts.items()):
@@ -248,43 +262,46 @@ for k,v in sorted(possible_starts.items()):
     mse_prev_video = np.mean( (X_test[i:i+n_mini_clips, :-1] - X_test[i:i+n_mini_clips, 1:])**2 ).item()
     mse_err_prev_video = np.mean( (X_hat[i:i+n_mini_clips, 1:-1] - X_hat[i:i+n_mini_clips, 2:])**2 ).item()
 
-    mse_model_video_sd = np.std( (X_test[i:i+n_mini_clips, 1:] - X_hat[i:i+n_mini_clips, 1:])**2 ).item()
-    mse_prev_video_sd = np.std( (X_test[i:i+n_mini_clips, :-1] - X_test[i:i+n_mini_clips, 1:])**2 ).item()
-    mse_err_prev_video_sd = np.std( (X_hat[i:i+n_mini_clips, 1:-1] - X_hat[i:i+n_mini_clips, 2:])**2 ).item()
+    mse_model_video_sd = np.std( (X_test[i:i+n_mini_clips, 1:] - X_hat[i:i+n_mini_clips, 1:])).item()
+    mse_prev_video_sd = np.std( (X_test[i:i+n_mini_clips, :-1] - X_test[i:i+n_mini_clips, 1:])).item()
+    mse_err_prev_video_sd = np.std( (X_hat[i:i+n_mini_clips, 1:-1] - X_hat[i:i+n_mini_clips, 2:])).item()
 
-    mse_model_video_var = np.var( (X_test[i:i+n_mini_clips, 1:] - X_hat[i:i+n_mini_clips, 1:])**2 ).item()
-    mse_prev_video_var = np.var( (X_test[i:i+n_mini_clips, :-1] - X_test[i:i+n_mini_clips, 1:])**2 ).item()
-    mse_err_prev_video_var = np.var( (X_hat[i:i+n_mini_clips, 1:-1] - X_hat[i:i+n_mini_clips, 2:])**2 ).item()
+#    mse_model_video_var = np.var( (X_test[i:i+n_mini_clips, 1:] - X_hat[i:i+n_mini_clips, 1:])**2 ).item()
+#    mse_prev_video_var = np.var( (X_test[i:i+n_mini_clips, :-1] - X_test[i:i+n_mini_clips, 1:])**2 ).item()
+#    mse_err_prev_video_var = np.var( (X_hat[i:i+n_mini_clips, 1:-1] - X_hat[i:i+n_mini_clips, 2:])**2 ).item()
 
     for j in range(n_mini_clips):
         for z in range(1,nt):
             mse_model_frame[k].append(np.mean( (X_test[i+j, z, :] - X_hat[i+j, z, :])**2 ).item())
             mse_prev_frame[k].append(np.mean( (X_test[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
 
-            mse_model_frame_sd[k].append(np.std( (X_test[i+j, z, :] - X_hat[i+j, z, :])**2 ).item())
-            mse_prev_frame_sd[k].append(np.std( (X_test[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
+            mse_model_frame_sd[k].append(np.std( (X_test[i+j, z, :] - X_hat[i+j, z, :])).item())
+            mse_prev_frame_sd[k].append(np.std( (X_test[i+j, z-1, :] - X_test[i+j, z, :])).item())
 
-            mse_model_frame_var[k].append(np.var( (X_test[i+j, z, :] - X_hat[i+j, z, :])**2 ).item())
-            mse_prev_frame_var[k].append(np.var( (X_test[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
+#            mse_model_frame_var[k].append(np.var( (X_test[i+j, z, :] - X_hat[i+j, z, :])**2 ).item())
+#            mse_prev_frame_var[k].append(np.var( (X_test[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
+
+            psnr_model_frame[k].append(psnr(np.mean( (X_test[i+j, z, :] - X_hat[i+j, z, :])**2 )))
+            psnr_prev_frame[k].append(psnr(np.mean( (X_test[i+j, z-1, :] - X_test[i+j, z, :])**2 )))
 
             if z > 1:
                 mse_err_prev_frame[k].append(np.mean( (X_hat[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
-                mse_err_prev_frame_sd[k].append(np.std( (X_hat[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
-                mse_err_prev_frame_var[k].append(np.var( (X_hat[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
+                mse_err_prev_frame_sd[k].append(np.std( (X_hat[i+j, z-1, :] - X_test[i+j, z, :])).item())
+#                mse_err_prev_frame_var[k].append(np.var( (X_hat[i+j, z-1, :] - X_test[i+j, z, :])**2 ).item())
                 
     mse_videos[k] = (mse_model_video, mse_prev_video, mse_err_prev_video)
     mse_videos_sd[k] = (mse_model_video_sd, mse_prev_video_sd, mse_err_prev_video_sd)
-    mse_videos_var[k] = (mse_model_video_var, mse_prev_video_var, mse_err_prev_video_var)
+#    mse_videos_var[k] = (mse_model_video_var, mse_prev_video_var, mse_err_prev_video_var)
     i += n_mini_clips
 
 mse_model = np.mean( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
 mse_prev = np.mean( (X_test[:, :-1] - X_test[:, 1:])**2 )
 
-mse_model_sd = np.std( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
-mse_prev_sd = np.std( (X_test[:, :-1] - X_test[:, 1:])**2 )
+mse_model_sd = np.std( (X_test[:, 1:] - X_hat[:, 1:]))  # look at all timesteps except the first
+mse_prev_sd = np.std( (X_test[:, :-1] - X_test[:, 1:]))
 
-mse_model_var = np.var( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
-mse_prev_var = np.var( (X_test[:, :-1] - X_test[:, 1:])**2 )
+#mse_model_var = np.var( (X_test[:, 1:] - X_hat[:, 1:])**2 )  # look at all timesteps except the first
+#mse_prev_var = np.var( (X_test[:, :-1] - X_test[:, 1:])**2 )
     
 with open(mse_videos_path, 'w') as fp:
     json.dump(mse_videos, fp, sort_keys=True, indent=4)
@@ -310,17 +327,23 @@ with open(mse_prev_frame_sd_path, 'w') as fp:
 with open(mse_err_prev_frame_sd_path, 'w') as fp:
     json.dump(mse_err_prev_frame_sd, fp, sort_keys=True, indent=4)    
 
-with open(mse_videos_var_path, 'w') as fp:
-    json.dump(mse_videos_var, fp, sort_keys=True, indent=4)
+#with open(mse_videos_var_path, 'w') as fp:
+#    json.dump(mse_videos_var, fp, sort_keys=True, indent=4)
+#
+#with open(mse_frame_var_path, 'w') as fp:
+#    json.dump(mse_model_frame_var, fp, sort_keys=True, indent=4)
+#
+#with open(mse_prev_frame_var_path, 'w') as fp:
+#    json.dump(mse_prev_frame_var, fp, sort_keys=True, indent=4)
+#
+#with open(mse_err_prev_frame_var_path, 'w') as fp:
+#    json.dump(mse_err_prev_frame_var, fp, sort_keys=True, indent=4)    
 
-with open(mse_frame_var_path, 'w') as fp:
-    json.dump(mse_model_frame_var, fp, sort_keys=True, indent=4)
+with open(psnr_frame_path, 'w') as fp:
+    json.dump(psnr_model_frame, fp, sort_keys=True, indent=4)
 
-with open(mse_prev_frame_var_path, 'w') as fp:
-    json.dump(mse_prev_frame_var, fp, sort_keys=True, indent=4)
-
-with open(mse_err_prev_frame_var_path, 'w') as fp:
-    json.dump(mse_err_prev_frame_var, fp, sort_keys=True, indent=4)    
+with open(psnr_prev_frame_path, 'w') as fp:
+    json.dump(psnr_prev_frame, fp, sort_keys=True, indent=4)
 
 #np.save(Xhat_path, X_hat)
 #np.save(Xtest_path, X_test)
@@ -331,8 +354,8 @@ f.write("Model MSE: %f\n" % mse_model)
 f.write("Previous Frame MSE: %f\n" % mse_prev)
 f.write("Model SDE: %f\n" % mse_model_sd)
 f.write("Previous Frame SDE: %f\n" % mse_prev_sd)
-f.write("Model VAR: %f\n" % mse_model_var)
-f.write("Previous Frame VAR: %f\n" % mse_prev_var)
+#f.write("Model VAR: %f\n" % mse_model_var)
+#f.write("Previous Frame VAR: %f\n" % mse_prev_var)
 f.close()
 #compare_results(pred_save_path, X_test, X_hat, nt)
 make_error_plot(mse_model_frame, err_save_path)
@@ -341,5 +364,6 @@ make_error_plot(mse_model_frame, err_save_path)
 make_error_plot(mse_prev_frame, err_prev_save_path)
 make_error_plot(mse_err_prev_frame, err_model_prev_save_path)
 
-make_error_plot(mse_model_frame_var, var_save_path)
+#make_error_plot(mse_model_frame_var, var_save_path)
 make_error_plot(mse_model_frame_sd, sd_save_path)
+make_error_plot(psnr_model_frame, psnr_save_path)
